5cee7e83f3e9425ec852d0e3a2574148f75db89e
maver1ck2022 <113671350+maver1ck2022@users.noreply.github.com>
2023-03-09 11:12:21 +0000

    Add files via upload

---
+++ AzureAD-Set-LocalAdmin.ps1
@@ -0,0 +1,8 @@
+﻿#Author: James Millard
+#Date: 09/03/2023
+#Note: The purpose of this script is assigned a logged on user to the local administrator roll.
+#This should be assigned to the device and once executed the device removed from the assigning group to prevent accidental deployments.
+
+
+Add-LocalGroupMember -Group "Administrators" -Member (Get-WMIObject -ClassName Win32_ComputerSystem).Username
+New-Item -Path "C:\Windows\debug\AAD-Admin.txt"
